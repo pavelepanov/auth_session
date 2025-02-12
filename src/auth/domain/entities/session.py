f49b113ab6858f13df60a794858d02d97b3e354c
Pavel Epanov <pavelepanov@MacBook-Air-Pavel.local>
2025-02-12 17:45:56 +0500

    Add expiration to model session

--- src/auth/domain/entities/session.py
+++ src/auth/domain/entities/session.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from datetime import datetime
 from typing import NewType
 
 from auth.domain.entities.user import UserId
@@ -9,14 +10,17 @@ SessionId = NewType("SessionId", str)
 @dataclass
 class Session:
     id: SessionId
+    expiration: datetime
     user_id: UserId
 
 
 def create_session(
     id: SessionId,
+    expiration: datetime,
     user_id: UserId,
 ) -> Session:
     return Session(
         id=id,
+        expiration=expiration,
         user_id=user_id,
     )
