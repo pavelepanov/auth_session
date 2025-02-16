34999474be228078ab3a53cca9f07811bcabd8fe
Pavel Epanov <pavelepanov@MacBook-Air-Pavel.local>
2025-02-16 21:50:48 +0500

    Add roles to domain

--- src/auth/domain/entities/user.py
+++ src/auth/domain/entities/user.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 from typing import NewType
 from uuid import UUID
 
+from auth.domain.enums import UserRoleEnum
+
 UserId = NewType("UserId", UUID)
 UserName = NewType("Username", str)
 PasswordHash = NewType("PasswordHash", str)
@@ -15,12 +17,16 @@ class User:
     username: UserName
     password_hash: PasswordHash
     is_active: bool
+    role: UserRoleEnum
 
 
-def create_user(id: UserId, username: UserName, password_hash: PasswordHash) -> User:
+def create_user(
+    id: UserId, username: UserName, password_hash: PasswordHash, role: UserRoleEnum
+) -> User:
     return User(
         id=id,
         username=username,
         password_hash=password_hash,
         is_active=True,
+        role=role,
     )
